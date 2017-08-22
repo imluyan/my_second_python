96c9b3ee12eac4ae24cc841839082c33b062e3e5
卢炎 <imluyan@icloud.com>
2017-08-23 00:21:18 +0800

    just work!

--- app.py
+++ app.py
@@ -1,27 +1,18 @@
 # -*- coding: utf-8 -*-
-import sqlite3
-import ssl
-import urllib.request, urllib.error
-from bs4 import BeautifulSoup
-import re
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import os
 import time
 
 url = 'https://www.mql5.com/zh/trading'
 driver = webdriver.Safari()
 driver.get(url)
+time.sleep(10)
 driver.switch_to.frame('webTerminalHost')
-try:
-    element = WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.ID, 'GBPUSD'))
-    )
-    money_element_lst = driver.find_elements_by_tag_name('tbody')
-    for every in money_element_lst:
-        print(every)
+while True:
+    body_element = driver.find_element_by_tag_name('tbody')
+    forex_element_lst = body_element.find_elements_by_tag_name('tr')
 
-finally:
-    driver.quit()
+    for forex_element in forex_element_lst:
+        forex = forex_element.get_attribute('id')
+        price_bid = forex_element.find_element_by_id('bid')
+        price_ask = forex_element.find_element_by_id('ask')
+        print(forex, price_bid.text, price_ask.text)
